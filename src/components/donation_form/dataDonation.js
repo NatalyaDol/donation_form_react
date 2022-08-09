5fb8f6d0119c706f97e4d8eab2447f2233c372b2
NatalyaDol <suhoikova@gmail.com>
2022-08-09 16:31:14 -0400

    Add new date in data

--- src/components/donation_form/dataDonation.js
+++ src/components/donation_form/dataDonation.js
@@ -1,6 +1,6 @@
 const DataDonation = {
     targetDonateAmount: 500,
-    endDate: '2022-08-11',
+    endDate: '2022-09-11',
     minDonate: 10,
     btnSaveContent: 'Save for later',
     btnTellContent: 'Tell your friends'
